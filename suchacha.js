53a5b5f5de1b59e953c72e55d7f1dcaf2a4a3d5a
sso just <528226249@qq.com>
2019-12-28 23:53:21 +0800

    feat(z2e): translate.js

--- suchacha.js
+++ suchacha.js
@@ -5,15 +5,19 @@ const fs = require('fs');
 const axios = require('axios');
 const chalk = require('chalk');
 const translate = require('./translate').E2Z; //英文句子翻译成中文
+const z2etranslate = require('./translate').Z2E;  //中文翻译成英文
 const word_url = 'https://api.shanbay.com/bdc/search/?word='; //旧版单词查询API
 const new_word_url = 'https://www.shanbay.com/api/v1/bdc/search/?version=2&word=';  //新版单词查询APIs
 const log = console.log;
 
 program
-    .version('2.0.2', '-v, --version')
+    .version('2.0.3', '-v, --version')
     .option('-w --word [word]', 'english of word')
-    .option('-s --sentence [sentence]', '要进行翻译的英文句子')
+    .option('-e2z --sentence [e2z]', '英文翻译为中文')
+    .option('-z2e [z2e]', '中文翻译为英文')
     .parse(process.argv);
+
+console.log(program, 20);
 
 if (program.word) {
     (async function f() {
@@ -28,20 +32,32 @@ if (program.word) {
                     console.log(item.pos, item.defn);
                 });
             }
-        }catch (err) {
+        } catch (err) {
             console.log('你输入的单词不正确');
         }
     })();
 }
 
-if (program.sentence) {
+if (program.e2z) {
     (async function f() {
-        let q = program.sentence;
-        let data = await translate(q);
-        console.log(data, 41);
-        await log(chalk.magenta.underline(program.sentence));
-        console.log(data.translation[0]);
+        try {
+            let q = program.e2z;
+            let data = await translate(q);
+            await log(chalk.magenta.underline(program.e2z));
+            console.log(data.translation[0]);
+        } catch (e) {
+            console.log(e.message)
+        }
     })();
-
 }
 
+if (program.z2e) {
+    ( async function f() {
+        try{
+            let  q = program.z2e;
+            z2etranslate(q);
+        }catch (e) {
+            console.log(e.message)
+        }
+    })()
+}
